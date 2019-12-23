0369f9dcb2078d3af6d1d8281398ab62befe06d6
Maynard Hartman <mjhartman@brighthouse.com>
2019-12-23 10:08:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 require "pry"
 def reverse_each_word(originalstring)
-    #define some vairables we will need
     
     originalstring.split.collect {|word| word.reverse}.join(" ")
     
-    binding.pry
+    #binding.pry
 end
